0aed9d87691bc048113dc1ea8cc8866e2fafd79c
Stalin32 <denerko32@yandex.ru>
2022-06-30 07:16:24 +0300

    лотерея из рандома

---
+++ lottery.py
@@ -0,0 +1,20 @@
+import random
+
+spisok = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+
+
+my_ticket = [1, 3, 5, 12]
+print(f"У вас билет с элементами {my_ticket}.")
+
+count = 1
+k = random.sample(spisok, 4)
+k.sort
+while my_ticket != k:
+    count +=1
+    k = random.sample(spisok, 4)
+    k.sort
+    print(k)
+
+
+print(f"Чтобы выиграть, вам потребовалось {count} попыток")
+
---
+++ my_restaurant.py
@@ -0,0 +1,4 @@
+import restaurant as r
+
+my_restaurant = r.Restaurant('seVen', 'просто вкусно')
+my_restaurant.describe_restaurant()
